c4c220e97b524161be008bf67e0691e24703e1d8
dodinhtrung97 <dodinhtrung97@gmail.com>
2016-12-03 21:18:08 +0700

    Finished with extracting Frequency

--- ExtractFrequency.py
+++ ExtractFrequency.py
@@ -1,3 +1,7 @@
+from numpy import argmax, mean, diff
+from matplotlib.mlab import find
+from scipy.signal import fftconvolve
+
 # Calculate position of a sample maxima given nearby samples
 # Use quadratic interpolation
 
@@ -18,3 +22,31 @@ def findMaxima(vector, index):
     # Return position of the vertex of the parabola that goes through x and its neighbors
 
     return posX, posY
+
+# Calculate frequency with auto-correlation
+# Note: Does not work for musical instruments, according to the internet
+
+
+def extractFrequency_autocor(signal, frameRate):
+
+    # Calculate auto-correlation, discount negative lags
+
+    # Remove DC offset to get rid of potential "triangular trend"
+
+    signal -= mean(signal)
+
+    # Convolve signal and signal[::1] using built in fast Fourier
+
+    cor = fftconvolve(signal, signal[::-1], mode='full')  # Output size 'full', output the full convolution of inputs
+    cor = cor[len(cor)/2:]
+
+    # Detect first Minima
+    start = find(diff(cor) > 0)[0]
+
+    # Detect peak after first Minima
+    # Unreliable for long signals (huge file)
+
+    peak = argmax(cor[start:]) + start  # Peak might occur between samples
+    interpole = findMaxima(cor, peak)[0]
+
+    return frameRate / interpole
